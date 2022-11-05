9eab8b80672df443e779125ce2a199473e6f5663
SungYoon Hong <hsy0113@korea.ac.kr>
2022-11-05 15:30:58 +0900

    :sparkles: 간단한 주문조회v4 - JPA에서 DTO로 바로 조회

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -30,8 +30,8 @@ public class OrderRepository {
 
     public List<Order> findAllByString(OrderSearch orderSearch) {
 
-            String jpql = "select o from Order o join o.member m";
-            boolean isFirstCondition = true;
+        String jpql = "select o from Order o join o.member m";
+        boolean isFirstCondition = true;
 
         //주문 상태 검색
         if (orderSearch.getOrderStatus() != null) {
