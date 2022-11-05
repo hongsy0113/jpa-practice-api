bd5891bc5318b2736ad13f8871a52d337951a45a
SungYoon Hong <hsy0113@korea.ac.kr>
2022-11-05 15:56:40 +0900

    :sparkles: 주문 조회V1 - 엔티티 직접 노출

---
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,38 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    /**
+     * V1. 엔티티 직접 노출
+     * - 엔티티가 변하면 API 스펙이 변한다.
+     * - 트랜잭션 안에서 지연 로딩 필요
+     * - 양방향 연관관계 문제
+     */
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream()
+                    .forEach(o -> o.getItem().getName()); // 강제 초기화
+        }
+        return all;
+    }
+}
